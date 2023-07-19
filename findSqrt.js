e2e8de534737ed6987ebb48c533e578d12aa1ee8
snsolomar <snsolomar@gmail.com>
2023-07-18 23:15:07 -0500

    practice findsqrt

--- findSqrt.js
+++ findSqrt.js
@@ -1,6 +1,6 @@
-//Given a non-negative integer x, 
-//return the square root of x rounded down to the nearest integer. 
-//The returned integer should be non-negative as well.
+// Given a non-negative integer x, 
+// return the square root of x rounded down to the nearest integer. 
+// The returned integer should be non-negative as well.
 
 var mySqrt = function(x) {
     // For special cases when x is 0 or 1, return x.
@@ -32,3 +32,27 @@ var mySqrt = function(x) {
 
 let k = 81;
 console.log(mySqrt(k));
+
+var mySqrt = function(x) {
+    if (x < 2){
+        return x;
+    }
+
+    let left = 2;
+    let right = x;
+
+    while (left < right){
+        let mid = Math.floor((left + right) / 2);
+        let squared = mid * mid;
+
+        if (x === squared){
+            return mid;
+        }
+        if (x < squared){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return right;
+}
